eb5401fbf76bdd1e74327bd70ed3d5ece5d19cfa
jwilsontt <jwilsontt@gmail.com>
2017-04-25 12:42:35 -0400

    Initial

---
+++ isocc/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+from .models import CountryIso
+# Register your models here.
+
+class CountryAdmin(admin.ModelAdmin):
+	list_display = ('country_name', 'alpha_2', 'alpha_3')
+
+	def country_name(self, obj):
+		return obj.country_name
+
+	country_name.admin_order_field = 'country_name'
+
+admin.site.register(CountryIso, CountryAdmin)
---
+++ isocc/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils.encoding import python_2_unicode_compatible
+
+# Create your models here.
+
+class CountryIso(models.Model):
+	country_name = models.CharField(max_length=200)
+	alpha_2 = models.CharField(max_length=2)
+	alpha_3 = models.CharField(max_length=3)
+	un_m49_num = models.IntegerField(default=0)
+
+	def __str__(self):
+		return self.country_name
