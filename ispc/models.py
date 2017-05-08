4a5ff2c53773fcb0021540cfe545a7d19024ac17
jwilsontt <jwilsontt@gmail.com>
2017-05-08 15:39:09 -0400

    Not sure

--- ispc/models.py
+++ ispc/models.py
@@ -5,10 +5,13 @@ from isocc.models import CountryIso
 
 class ISPointCodes(models.Model):
 	ispc = models.CharField(max_length=12)
-	dec = models.IntegerField()
+	dec = models.IntegerField(default=1)
 	sigpoint_name = models.CharField(null=True, blank=True, max_length=100)
 	sigpoint_operator = models.CharField(max_length=250)
 	sigpoint_country = models.ForeignKey('isocc.CountryIso', on_delete=models.CASCADE)
+
+	class Meta:
+		ordering = ["ispc"]
 
 	def __str__(self):
 		return self.ispc
